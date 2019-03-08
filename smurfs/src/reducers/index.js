7dd49a73ec7a324b7dcbe5a0d8edcb8fd0afbab3
Max Trestman <mtrestman1@android-4105f9b98c1d7f96.workspace.codeandsupply.co>
2019-03-08 11:32:56 -0500

    Updated index.js, added actions (still need to add post), imported actions to reducers/set intialState

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,24 @@
+import {
+FETCH_SMURFS_START,
+FETCH_SMURFS_SUCCESS, 
+FETCH_SMURFS_FAILURE,
+ADD_SMURFS
+} from '../actions/index';
+
+
+
 /*
   Be sure to import in all of the action types from `../actions`
 */
-
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+  const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
  }
-*/
+
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +27,10 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+export const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: 
+    return state;
+  }
+}
